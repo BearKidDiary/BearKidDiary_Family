19c37545e3439a5707d12acf0697b2b849e87ebc
SCNUzy <975135274@qq.com>
2016-07-04 15:19:14 +0800

    BaseActivity

--- app/src/main/java/bearkid/com/bearkiddiaryfamily/global/BaseApplication.java
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/global/BaseApplication.java
@@ -5,17 +5,35 @@ import android.app.Application;
 
 import java.util.Stack;
 
+import bearkid.com.bearkiddiaryfamily.ui.activity.BaseActivity;
+
 /**
  * Created by admin on 2016/7/4.
  */
 public class BaseApplication extends Application{
 
-    Stack<Activity> activityStack;
+    Stack<BaseActivity> activityStack;
 
     @Override
     public void onCreate() {
         super.onCreate();
     }
 
+    /**
+     * 退出整个程序
+     * 在这里写释放SDK资源等代码
+     */
+    public void exit(){
+        while(activityStack.size()!=0){
+            activityStack.peek().finish();
+        }
+    }
 
+    public void pushActivity(BaseActivity activity){
+        activityStack.push(activity);
+    }
+
+    public void popActivity(){
+        activityStack.pop();
+    }
 }
--- app/src/main/java/bearkid/com/bearkiddiaryfamily/ui/activity/BaseActivity.java
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/ui/activity/BaseActivity.java
@@ -1,10 +1,24 @@
 package bearkid.com.bearkiddiaryfamily.ui.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+
+import bearkid.com.bearkiddiaryfamily.global.BaseApplication;
 
 /**
  * Created by admin on 2016/7/4.
  */
 public class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        ((BaseApplication)getApplication()).pushActivity(this);
+    }
 
+    @Override
+    public void finish() {
+        super.finish();
+        ((BaseApplication)getApplication()).popActivity();
+    }
 }
