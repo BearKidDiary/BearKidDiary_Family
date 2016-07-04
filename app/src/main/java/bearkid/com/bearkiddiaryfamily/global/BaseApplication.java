a8b57e3f9939ac6d59a5e2a5a004e28b898f6180
SCNUzy <975135274@qq.com>
2016-07-04 17:40:28 +0800

    添加Bmob的依赖

--- app/src/main/java/bearkid/com/bearkiddiaryfamily/global/BaseApplication.java
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/global/BaseApplication.java
@@ -6,6 +6,7 @@ import android.app.Application;
 import java.util.Stack;
 
 import bearkid.com.bearkiddiaryfamily.ui.activity.BaseActivity;
+import cn.bmob.v3.Bmob;
 
 /**
  * Created by admin on 2016/7/4.
@@ -17,6 +18,23 @@ public class BaseApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        activityStack = new Stack<>();
+
+        //第一：默认初始化
+        Bmob.initialize(this, "Your Application ID");
+
+        //第二：自v3.4.7版本开始,设置BmobConfig,允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)，
+        //BmobConfig config =new BmobConfig.Builder(this)
+        ////设置appkey
+        //.setApplicationId("Your Application ID")
+        ////请求超时时间（单位为秒）：默认15s
+        //.setConnectTimeout(30)
+        ////文件分片上传时每片的大小（单位字节），默认512*1024
+        //.setUploadBlockSize(1024*1024)
+        ////文件的过期时间(单位为秒)：默认1800s
+        //.setFileExpiration(2500)
+        //.build();
+        //Bmob.initialize(config);
     }
 
     /**
---
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/model/bean/Organization.java
@@ -0,0 +1,71 @@
+package bearkid.com.bearkiddiaryfamily.model.bean;
+
+import cn.bmob.v3.BmobObject;
+import cn.bmob.v3.datatype.BmobDate;
+import cn.bmob.v3.datatype.BmobFile;
+
+/**
+ * Created by admin on 2016/7/4.
+ */
+public class Organization extends BmobObject{
+    /**
+     * 机构的名称
+     */
+    private String Oname;
+    /**
+     * 机构的地址
+     */
+    private String Oaddress;
+    /**
+     * 机构的成立时间
+     */
+    private BmobDate Otime;
+    /**
+     * 机构的标志图
+     */
+    private BmobFile Oavatar;
+    /**
+     * 机构的公告
+     */
+    private String Oannounce;
+
+    public String getOname() {
+        return Oname;
+    }
+
+    public void setOname(String oname) {
+        Oname = oname;
+    }
+
+    public String getOaddress() {
+        return Oaddress;
+    }
+
+    public void setOaddress(String oaddress) {
+        Oaddress = oaddress;
+    }
+
+    public BmobDate getOtime() {
+        return Otime;
+    }
+
+    public void setOtime(BmobDate otime) {
+        Otime = otime;
+    }
+
+    public BmobFile getOavatar() {
+        return Oavatar;
+    }
+
+    public void setOavatar(BmobFile oavatar) {
+        Oavatar = oavatar;
+    }
+
+    public String getOannounce() {
+        return Oannounce;
+    }
+
+    public void setOannounce(String oannounce) {
+        Oannounce = oannounce;
+    }
+}
