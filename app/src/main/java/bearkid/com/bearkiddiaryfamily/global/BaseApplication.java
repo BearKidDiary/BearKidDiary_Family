f28e6975203932a4928e4666d9e7f12a85feea48
SCNUzy <975135274@qq.com>
2016-07-04 15:11:08 +0800

    BaseActivity

--- app/src/main/java/bearkid/com/bearkiddiaryfamily/global/BaseApplication.java
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/global/BaseApplication.java
@@ -1,9 +1,21 @@
 package bearkid.com.bearkiddiaryfamily.global;
 
+import android.app.Activity;
 import android.app.Application;
+
+import java.util.Stack;
 
 /**
  * Created by admin on 2016/7/4.
  */
 public class BaseApplication extends Application{
+
+    Stack<Activity> activityStack;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
 }
---
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/ui/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package bearkid.com.bearkiddiaryfamily.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2016/7/4.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
