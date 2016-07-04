53ebdaaf4990561505ccbad59d57577535d0afa6
SCNUzy <975135274@qq.com>
2016-07-04 14:52:47 +0800

    新建目录结构

---
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/model/MainModel.java
@@ -0,0 +1,7 @@
+package bearkid.com.bearkiddiaryfamily.model;
+
+/**
+ * Created by admin on 2016/7/4.
+ */
+public class MainModel {
+}
---
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/presenter/MainPresenter.java
@@ -0,0 +1,7 @@
+package bearkid.com.bearkiddiaryfamily.presenter;
+
+/**
+ * Created by admin on 2016/7/4.
+ */
+public class MainPresenter {
+}
