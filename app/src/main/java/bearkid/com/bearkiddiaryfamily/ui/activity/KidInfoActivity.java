31ed0edc9423d9316e7512e2fd6da3c7a6bb5b92
yarenChoi <786647819@qq.com>
2016-07-14 17:48:34 +0800

    孩子信息界面

---
+++ app/src/main/java/bearkid/com/bearkiddiaryfamily/ui/activity/KidInfoActivity.java
@@ -0,0 +1,14 @@
+package bearkid.com.bearkiddiaryfamily.ui.activity;
+
+import android.os.Bundle;
+
+import bearkid.com.bearkiddiaryfamily.R;
+
+public class KidInfoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kid_info);
+    }
+}
